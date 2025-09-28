c5c5542674894adffa7a5821297682b118bac7af
genesisvaca <138408577+genesisvaca@users.noreply.github.com>
2025-09-28 19:26:21 +0200

    feat: SQL Operators

---
+++ Operators.sql
@@ -0,0 +1,38 @@
+select * 
+from "Employee";
+
+-- ID igual que 3
+select "EmployeeId", "FirstName" , "LastName" 
+from "Employee"
+where "EmployeeId" = 3;
+
+-- ID mayor que 5
+select "EmployeeId", "FirstName" , "LastName" 
+from "Employee"
+where "EmployeeId" > 5;
+
+-- ID mayor o igual que 5
+select "EmployeeId", "FirstName" , "LastName" 
+from "Employee"
+where "EmployeeId" >= 5;
+
+-- ID Distinto que 5
+select "EmployeeId", "FirstName" , "LastName" 
+from "Employee"
+where "EmployeeId" <> 5;
+
+-- Select con fecha de nacimiento menor que '1970-05-29'
+select "EmployeeId", "FirstName", "LastName", "BirthDate" 
+from "Employee"
+where "BirthDate" < '1970-05-29';
+
+-- Todos los empleados de Calgary
+select * 
+from "Employee"
+where "City" = 'Calgary';
+
+-- Todos los empleados que no sean de Calgary
+select * 
+from "Employee"
+where "City" <> 'Calgary';
+
