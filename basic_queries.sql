d6c5185100d21fe76d1a3715d08b071304fd1770
genesisvaca <138408577+genesisvaca@users.noreply.github.com>
2025-09-28 18:55:47 +0200

    feat: Basic Queries

---
+++ basic_queries.sql
@@ -0,0 +1,14 @@
+--EN ESTA QUERY ESTAMOS SELECCIONANDO LAS COLUMNAS DE LA TABLA ALBUM
+select * 
+from "Album";
+
+
+/* QUERY QUE SELECCIONA EL GENERO DE LA TABLA*/
+select "GenreId"
+from "Track";
+
+select "FirstName", "LastName", "City", "BirthDate" 
+from "Employee";
+
+select "BirthDate", "FirstName", "LastName", "City"
+from "Employee";
